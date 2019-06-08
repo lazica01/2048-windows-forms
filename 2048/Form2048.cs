f7b057d8ac86a63f0498e41d916a2084cfab7cf6
Lazar <lazar.stojanovic322@gmail.com>
2019-06-08 15:05:43 +0200

    Startup commit

---
+++ 2048/Form2048.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2048
+{
+    public partial class Form2048 : Form
+    {
+        public static Dictionary<String, Image> images;
+        public Form2048()
+        {
+            InitializeComponent();
+            images = LoadImages(@"..\..\..\Slike");
+            this.Size = new Size(530, 740);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+        public Dictionary<String, Image> LoadImages(string path)
+        {
+            Dictionary<String, Image> images = new Dictionary<String, Image>();
+            string[] buffer = Directory.GetFiles(path, "*.png");
+            foreach (string imagePath in buffer)
+            {
+                string[] parts = imagePath.Split('\\');
+                string name = parts[parts.Length - 1].Split('.')[0];
+                Image img = Image.FromFile(imagePath);
+                images.Add(name, img);
+            }
+            return images;
+
+        }
+
+        private void Form2048_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = images["background"];
+            Tile t = new Tile(0, 0, 2);
+            Controls.Add(t.pb);
+
+            t = new Tile(1, 1, 2);
+            Controls.Add(t.pb);
+
+        }
+
+        private void Form2048_MouseMove(object sender, MouseEventArgs e)
+        {
+            var relativePoint = this.PointToClient(Cursor.Position);
+            label1.Text = relativePoint.ToString();
+        }
+    }
+}
---
+++ 2048/Tile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2048
+{
+    class Tile
+    {
+        public static int offsetX = 60;
+        public static int offsetY = 250;
+
+        public PictureBox pb;
+        private int i, j;
+        private int value;
+
+        public Tile(int i, int j, int value)
+        {
+            pb = new PictureBox();
+            pb.Size = new Size(90, 90);
+            this.i = i;
+            this.j = j;
+            ChangePosition(i, j);
+            ChangePosition(i+2, j+2);
+
+            UpdateImage(value);
+        }
+     
+        public void UpdateImage(int value)
+        {
+            this.value = value;
+            pb.Image = Form2048.images[value.ToString()];
+        }
+        private void ChangePosition(int i, int j)
+        {
+            pb.Location = new Point(offsetX + j * 100, offsetY + i * 100);
+            //ovo ce da bude mnogo komplikovanija funkcija sa animacijama
+            //     2|------------------>
+            //             1|---------->
+            // naprimer 1 i 2 ce u isto vreme da stignu na cilj treba malo matematikica ovde xD
+            // ovo na kraju implementiramo.
+            pb.Update();
+
+        }
+        /*
+        public void Up()
+        {
+            if(...)
+               UpdateImage(value)
+               
+          
+           ChangePosition(end_i, end_j);
+        }
+        public void Down()
+        {
+         * if(...)
+               UpdateImage(value)
+               
+          
+           ChangePosition(end_i, end_j);
+        }
+        public void Left()
+        {
+         * if(...)
+               UpdateImage(value)
+               
+          
+           ChangePosition(end_i, end_j);
+        }
+        public void Right()
+        {
+         * if(...)
+               UpdateImage(value)
+               
+          
+           ChangePosition(start_i, start_j, end_i, end_j);
+        }
+        */
+
+    }
+}
